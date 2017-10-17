1b270ae1cc11e22e7d15368ceb6aae315bced282
Timen Olthof <timen@timenolthof.nl>
2017-10-17 11:14:06 +0200

    refactor: use custom middleware instead of passport

--- src/actions/attributes-required.js
+++ src/actions/attributes-required.js
@@ -8,29 +8,28 @@ const qr = require('qr-image');
  * @param {object} res Express response object
  * @returns {undefined}
  */
-exports = module.exports = function requestHandler(req, res) {
+module.exports = function requestHandler(req, res) {
   const attribute = req.query.attribute;
   const attributesLabel = req.query.attributesLabel;
+  
   if (attribute && attributesLabel) {
     diva
       .startDisclosureSession(req.divaSessionState.user.sessionId, attribute, attributesLabel)
       .then((qrContent) => {
-        console.log(qrContent);
-        switch(req.query.type) {
+        switch (req.query.type) {
           case 'json':
           case 'qr':
           default:
-            let code = qr.image(qrContent, { type: 'png' });
             res.setHeader('Content-type', 'image/png');
             res.setHeader('Content-Disposition', 'inline; filename="filename.pdf"'); // Note: to force display in browser
-            code.pipe(res);
+            qr.image(qrContent, { type: 'png' }).pipe(res);
             break;
         }
       })
       .catch((error) => {
         res.end(error.toString());
-      })
+      });
   } else {
-    res.end('attribute or attributesLabel not set.')
+    res.end('attribute or attributesLabel not set.');
   }
-}
+};
