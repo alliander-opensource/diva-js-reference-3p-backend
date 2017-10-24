5d7535457cdedf13056a36a561f58ffad4dd19f3
Koen van Ingen <koenvaningen@gmail.com>
2017-10-24 14:29:23 +0200

    feat: add diva init call with custom config

--- src/server.js
+++ src/server.js
@@ -6,6 +6,21 @@ const diva = require('diva-irma-js');
 const simpleSession = require('./modules/simple-session');
 const config = require('./config');
 
+diva.init({
+  baseUrl: 'http://localhost:4000',
+  apiKey: 'FILL_IN',
+  irmaApiServerUrl: 'https://dev-diva-irma-api-server.appx.cloud',
+  irmaApiServerPublicKey: `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAql7fb0EMMkqKcXIuvCVb
+P+V1qV6AIzhxFlBO8k0GLogMUT6UXJSnXQ3P7iTIfr+/5+yf4dfKNHhalphe+2OB
+zspt6zymteKAuQ9/NwUNGTSP4l8mD8wQb5ZyiNMUt6leu42SPe/7uOtcRA6AzN2L
+6eKNqUGpNvQZTVwEFNNNiChqrkmQVnoyWVe6fHHooxTCtIyXWJY2WqC8lYStIbZc
+NP5xwUdLGOuGo41T7Q+wkR5KqXDif+FKoR7qlG7jEUHcbd1OQe7b6DxzSHCI65Bw
+TIZwMj2LtEwB6Op7vemHkeNaPAYK33t5kdyq+P55KMDuJgj+nxpFO00U4msD+CRa
+7QIDAQAB
+-----END PUBLIC KEY-----`,
+});
+
 const app = express();
 app.use(cookieParser(config.cookieSecret));
 app.use(cookieEncrypter(config.cookieSecret));
