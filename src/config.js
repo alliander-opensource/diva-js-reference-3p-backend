10472665e00378a928ab0e162da06d99e6b616e2
Koen van Ingen <koenvaningen@gmail.com>
2017-10-26 11:45:55 +0200

    feat: add k8s config for redis

--- src/config.js
+++ src/config.js
@@ -9,6 +9,22 @@ const config = {
     signed: true,
     secure: false, // TODO: NOTE: must be set to true and be used with HTTPS only!
   },
+  baseUrl: process.env.BASE_URL ? process.env.BASE_URL : 'http://localhost:4000',
+  apiKey: process.env.API_KEY ? process.env.API_KEY : 'FILL_IN',
+  irmaApiServerUrl: 'https://dev-diva-irma-api-server.appx.cloud',
+  irmaApiServerPublicKey: `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAql7fb0EMMkqKcXIuvCVb
+P+V1qV6AIzhxFlBO8k0GLogMUT6UXJSnXQ3P7iTIfr+/5+yf4dfKNHhalphe+2OB
+zspt6zymteKAuQ9/NwUNGTSP4l8mD8wQb5ZyiNMUt6leu42SPe/7uOtcRA6AzN2L
+6eKNqUGpNvQZTVwEFNNNiChqrkmQVnoyWVe6fHHooxTCtIyXWJY2WqC8lYStIbZc
+NP5xwUdLGOuGo41T7Q+wkR5KqXDif+FKoR7qlG7jEUHcbd1OQe7b6DxzSHCI65Bw
+TIZwMj2LtEwB6Op7vemHkeNaPAYK33t5kdyq+P55KMDuJgj+nxpFO00U4msD+CRa
+7QIDAQAB
+-----END PUBLIC KEY-----`,
+  useRedis: process.env.USE_REDIS ? process.env.USE_REDIS : false,
+  redisHost: process.env.REDIS_HOST ? process.env.REDIS_HOST : 'http://localhost',
+  redisPort: process.env.REDIS_PORT ? process.env.REDIS_PORT : '6379',
+  redisPassword: process.env.REDIS_PASSWORD ? process.env.REDIS_PASSWORD : '',
 };
 
 module.exports = config;
--- src/server.js
+++ src/server.js
@@ -7,19 +7,16 @@ const simpleSession = require('./modules/simple-session');
 const config = require('./config');
 
 diva.init({
-  baseUrl: 'http://localhost:4000',
-  apiKey: 'FILL_IN',
-  irmaApiServerUrl: 'https://dev-diva-irma-api-server.appx.cloud',
-  irmaApiServerPublicKey: `-----BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAql7fb0EMMkqKcXIuvCVb
-P+V1qV6AIzhxFlBO8k0GLogMUT6UXJSnXQ3P7iTIfr+/5+yf4dfKNHhalphe+2OB
-zspt6zymteKAuQ9/NwUNGTSP4l8mD8wQb5ZyiNMUt6leu42SPe/7uOtcRA6AzN2L
-6eKNqUGpNvQZTVwEFNNNiChqrkmQVnoyWVe6fHHooxTCtIyXWJY2WqC8lYStIbZc
-NP5xwUdLGOuGo41T7Q+wkR5KqXDif+FKoR7qlG7jEUHcbd1OQe7b6DxzSHCI65Bw
-TIZwMj2LtEwB6Op7vemHkeNaPAYK33t5kdyq+P55KMDuJgj+nxpFO00U4msD+CRa
-7QIDAQAB
------END PUBLIC KEY-----`,
-  useRedis: false,
+  baseUrl: config.baseUrl,
+  apiKey: config.apiKey,
+  irmaApiServerUrl: config.irmaApiServerUrl,
+  irmaApiServerPublicKey: config.irmaApiServerPublicKey,
+  useRedis: config.useRedis,
+  redisOptions: {
+    host: config.redisHost,
+    port: config.redisPort,
+    password: config.redisPassword,
+  },
 });
 
 const app = express();
