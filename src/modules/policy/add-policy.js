a186244875477ca896f1806300acd13c0a846752
Timen Olthof <timen@timenolthof.nl>
2018-02-26 17:17:59 +0100

    feat/wip: policy management in diva-ref-3p

---
+++ src/modules/policy/add-policy.js
@@ -0,0 +1,65 @@
+const diva = require('diva-irma-js');
+
+const Policy = require('../../database/models/Policy');
+
+/**
+ * Request handler
+ * @function requestHandler
+ * @param {object} req Express request object
+ * @param {object} res Express response object
+ * @returns {undefined}
+ */
+module.exports = function requestHandler(req, res) {
+  const sessionId = req.sessionId;
+  diva
+    .getAttributes(sessionId)
+    .then((attributes) => {
+      //TODO remove this as diva middleware makes sure this is not undefined
+      attributes['pbdf.pbdf.idin.address'] = ["Straat 1"]
+      attributes['pbdf.pbdf.idin.city'] = ["Stad"]
+      return {
+        street: attributes['pbdf.pbdf.idin.address'][0],
+        city: attributes['pbdf.pbdf.idin.city'][0],
+      };
+    })
+    .then(address => {
+      const { transaction_hash, service_provider, policy, message, irma_signature } = req.body;
+      return Policy.query().insert({
+        id: transaction_hash,
+        policy,
+        service_provider,
+        message,
+        irma_signature,
+        owner: address,
+      });
+      // return Policy.query().insert({
+      //   id: "62uhkjasgdk",
+      //   policy: {
+      //     actor: "Huishoudboekje",
+      //     action: "lezen",
+      //     actee: "mijn inkomensgegevens",
+      //     conditions: [],
+      //     goal: "om mijn financiën te regelen."
+      //   },
+      //   service_provider: "hhb",
+      //   message: "Huishoudboekje mag mijn inkomensgegevens lezen om mijn financiën te regelen.",
+      //   irma_signature: {},
+      //   owner: address,
+      // });
+    })
+    .then((policy) => {
+      res
+        .set({ Location: `${res.req.baseUrl}/${policy.id}` })
+        .status(201)
+        .send({
+          success: true,
+          message: 'Created',
+          id: policy.id,
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.end('Something went wrong.');
+    });
+
+};
