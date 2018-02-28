41bd3186de128cdb41b309a444b614df1258e430
Koen van Ingen <koenvaningen@gmail.com>
2018-02-28 14:39:56 +0100

    WIP refactor endpoints

---
+++ src/actions/start-irma-session.js
@@ -0,0 +1,33 @@
+const diva = require('diva-irma-js');
+
+/**
+ * Request handler for starting a new disclosure session via POST request
+ * @function requestHandler
+ * @param {object} req Express request object, containing an IRMA content object
+ * @param {object} res Express response object
+ * @returns {undefined}
+ */
+module.exports = function requestHandler(req, res) {
+  const content = req.body.content;
+  const message = req.body.message;
+  if (content) {
+    return Promise.resolve(() => {
+      if (message) {
+        return diva
+          .startSignatureSession(content, null, message)
+      } else {
+        return diva
+          .startDisclosureSession(req.sessionId, content)
+      }
+    })
+    .then((irmaSessionData) => {
+      res.setHeader('Content-type', 'application/json; charset=utf-8');
+      res.json(irmaSessionData);
+    })
+    .catch((error) => {
+      res.end(error.toString());
+    });
+  } else {
+    res.end('content not set.');
+  }
+};
