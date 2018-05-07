d7790afb220e8a1b3acdd176a828ea86e339d0ec
Koen van Ingen <koenvaningen@gmail.com>
2018-05-07 14:00:41 +0200

    chore: Remove policy support

--- src/server.js
+++ src/server.js
@@ -6,10 +6,6 @@ const diva = require('diva-irma-js');
 const simpleSession = require('./modules/simple-session');
 const config = require('./config');
 
-const initializeDatabase = require('./database/initialize-database');
-
-initializeDatabase(); // Required for policy saving
-
 diva.init({
   baseUrl: config.baseUrl,
   apiKey: config.apiKey,
@@ -35,7 +31,7 @@ app.use(simpleSession);
 app.get('/api/get-session', require('./actions/get-session'));
 app.get('/api/deauthenticate', require('./actions/deauthenticate'));
 
-// DIVA disclore endpoints
+// DIVA disclosure endpoints
 app.get('/api/start-disclosure-session', require('./actions/start-simple-disclosure-session'));
 app.post('/api/start-irma-session', require('./actions/start-irma-session'));
 app.get('/api/disclosure-status', require('./actions/disclosure-status'));
@@ -44,13 +40,6 @@ app.use('/api/images/address.jpg', diva.requireAttributes(['pbdf.pbdf.idin.addre
 
 // DIVA signature endpoints
 app.get('/api/signature-status', require('./actions/signature-status'));
-
-// Policy endpoints
-app.post('/api/policy/new', require('./modules/policy/add-policy'));
-app.get('/api/policy/all', diva.requireAttributes(['pbdf.pbdf.idin.address', 'pbdf.pbdf.idin.city']), require('./modules/policy/get-all-policies'));
-app.delete('/api/policy/:id', diva.requireAttributes(['pbdf.pbdf.idin.address', 'pbdf.pbdf.idin.city']), require('./modules/policy/delete-policy'));
-app.get('/api/policy/by-sp', require('./modules/policy/get-all-policies-by-sp'));
-app.post('/api/policy/get-message-for-policy', require('./modules/policy/get-message-for-policy'));
 
 const server = app.listen(config.port, () => {
   console.log(`Diva Reference Third Party backend listening on port ${config.port} !`); // eslint-disable-line no-console
